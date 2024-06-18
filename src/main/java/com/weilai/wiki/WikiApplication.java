9419a2464f2d441f0fbd29b39fc19006a8176bb3
miku-wwl <569144003@qq.com>
2024-06-18 22:36:02 +0800

    feat: 2-5 修改项目目录

--- src/main/java/com/weilai/wiki/WikiApplication.java
+++ src/main/java/com/weilai/wiki/WikiApplication.java
@@ -1,4 +1,4 @@
-package com.jiawa.wiki;
+package com.weilai.wiki;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/test/java/com/weilai/wiki/WikiApplicationTests.java
+++ src/test/java/com/weilai/wiki/WikiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jiawa.wiki;
+package com.weilai.wiki;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
