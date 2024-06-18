4fc7de5e3438f8a93ac7fab165a01151c47b0101
miku-wwl <569144003@qq.com>
2024-06-19 00:18:26 +0800

    feat: 3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/weilai/wiki/service/EbookService.java
+++ src/main/java/com/weilai/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.weilai.wiki.domain.EbookExample;
 import com.weilai.wiki.mapper.EbookMapper;
 import com.weilai.wiki.req.EbookReq;
 import com.weilai.wiki.resp.EbookResp;
+import com.weilai.wiki.util.CopyUtil;
 import jakarta.annotation.Resource;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -23,13 +24,15 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            EbookResp ebookResp = new EbookResp();
+//            BeanUtils.copyProperties(ebook, ebookResp);
+//            respList.add(ebookResp);
+//        }
+        // 列表复制
+        List<EbookResp> respList1 = CopyUtil.copyList(ebookList, EbookResp.class);
 
-        return respList;
+        return respList1;
     }
 }
