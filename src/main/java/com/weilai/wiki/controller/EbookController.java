48b8e6931cc4e78adf72bdbc15bd1abff0b7f87d
miku-wwl <569144003@qq.com>
2024-06-19 00:05:07 +0800

    feat: 3-6 增加通用的返回类CommonResp，以后所有后端接口的返回值全部都是CommonResp类型

--- src/main/java/com/weilai/wiki/controller/EbookController.java
+++ src/main/java/com/weilai/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.weilai.wiki.controller;
 
 import com.weilai.wiki.domain.Ebook;
+import com.weilai.wiki.resp.CommonResp;
 import com.weilai.wiki.service.EbookService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +17,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp<List<Ebook>> list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
