49ecb431176169604c70def9c41362cf6a01aa65
miku-wwl <569144003@qq.com>
2024-06-20 01:27:03 +0800

    feat: 5-8 增加拦截器，打印接口耗时

---
+++ src/main/java/com/weilai/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,18 @@
+package com.weilai.wiki.config;
+
+import com.weilai.wiki.interceptor.LogInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Resource
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");  //addPathPatterns()，要拦截请求;excludePathPatterns()，排除请求，不拦截
+    }
+}
