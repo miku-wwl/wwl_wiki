52f11fc946b7edfc2b5d8a396ea85703d9fd2b6c
miku-wwl <569144003@qq.com>
2024-06-25 23:27:37 +0800

    feat: 9-9 集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/weilai/wiki/controller/UserController.java
+++ src/main/java/com/weilai/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.weilai.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.weilai.wiki.req.UserLoginReq;
 import com.weilai.wiki.req.UserQueryReq;
 import com.weilai.wiki.req.UserResetPasswordReq;
@@ -9,17 +10,31 @@ import com.weilai.wiki.resp.PageResp;
 import com.weilai.wiki.resp.UserLoginResp;
 import com.weilai.wiki.resp.UserQueryResp;
 import com.weilai.wiki.service.UserService;
+import com.weilai.wiki.util.SnowFlake;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req) {
@@ -58,6 +73,11 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token：{}，并放入redis中", token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
         resp.setContent(userLoginResp);
         return resp;
     }
