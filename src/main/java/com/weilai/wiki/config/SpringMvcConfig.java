4847c02ec8bf588eb7da52d18362749c7efa0888
miku-wwl <569144003@qq.com>
2024-06-26 00:41:21 +0800

    feat: 9-11 前后端增加登录拦截 修复BUG：前后端存储token，统一用String类型

--- src/main/java/com/weilai/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/weilai/wiki/config/SpringMvcConfig.java
@@ -1,18 +1,28 @@
-//package com.weilai.wiki.config;
-//
-//import com.weilai.wiki.interceptor.LogInterceptor;
-//import jakarta.annotation.Resource;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//    @Resource
-//    LogInterceptor logInterceptor;
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");  //addPathPatterns()，要拦截请求;excludePathPatterns()，排除请求，不拦截
-//    }
-//}
+package com.weilai.wiki.config;
+
+import com.weilai.wiki.interceptor.LoginInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/find-content/**"
+                );
+    }
+}
