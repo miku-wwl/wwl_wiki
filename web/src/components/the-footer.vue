4d0dc55fe74cf4a2549681097582051c2bb31087
miku-wwl <569144003@qq.com>
2024-06-25 23:59:16 +0800

    feat: 9-9 使用vuex保存用户信息，方便其它组件获取登录信息。遗留问题，刷新后值没有了

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,12 +1,19 @@
 <template>
     <a-layout-footer style="text-align: center">
-      Weilai Wiki
+      Weilai Wiki，欢迎：{{ user.name }}
     </a-layout-footer>
   </template>
   
   <script lang="ts">
-  import {defineComponent} from 'vue';
+  import {computed, defineComponent} from 'vue';
+  import store from "@/store";
   export default defineComponent({
-    name: 'the-footer'
+    name: 'the-footer',
+    setup() {
+      const user = computed(() => store.state.user)
+      return {
+        user
+      }
+    }
   });
   </script>
