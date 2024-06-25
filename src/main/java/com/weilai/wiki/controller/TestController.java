f5d82c0b058f020c6639d66e00bea6c99954674c
miku-wwl <569144003@qq.com>
2024-06-25 23:38:29 +0800

    feat: 9-9 增加redis测试接口

--- src/main/java/com/weilai/wiki/controller/TestController.java
+++ src/main/java/com/weilai/wiki/controller/TestController.java
@@ -3,13 +3,19 @@ package com.weilai.wiki.controller;
 import com.weilai.wiki.domain.Test;
 import com.weilai.wiki.service.TestService;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
     // 优先读配置文件，没有就使用默认值
     @Value("${test.hello:TEST}")
@@ -17,6 +23,9 @@ public class TestController {
 
     @Resource
     private TestService testService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/hello")
     public String hello() {
@@ -31,5 +40,19 @@ public class TestController {
     @GetMapping("/test/list")
     public List<Test> list() {
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
