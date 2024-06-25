9953bcb0a0d343887ca7b5aeaa3aa25509bcee05
miku-wwl <569144003@qq.com>
2024-06-26 01:58:27 +0800

    feat: 10-5 定时任务示例 个文档点赞一次

--- src/main/java/com/weilai/wiki/config/WikiApplication.java
+++ src/main/java/com/weilai/wiki/config/WikiApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ComponentScan("com.weilai")
 @SpringBootApplication
 @MapperScan("com.weilai.wiki.mapper")
+@EnableScheduling
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
