6027aa534c5db625bd8ae9b25d936c2a2587d366
miku-wwl <569144003@qq.com>
2024-06-26 22:20:30 +0800

    feat: 10-6 增加定时器，定时执行电子书信息更新SQL

---
+++ src/main/java/com/weilai/wiki/job/DocJob.java
@@ -0,0 +1,25 @@
+package com.weilai.wiki.job;
+
+import com.weilai.wiki.service.DocService;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DocJob {
+    private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
+
+    @Resource
+    private DocService docService;
+
+    /**
+     * 每30秒更新电子书信息
+     */
+    @Scheduled(cron = "5/30 * * * * ?")
+    public void cron() {
+        docService.updateEbookInfo();
+    }
+
+}
