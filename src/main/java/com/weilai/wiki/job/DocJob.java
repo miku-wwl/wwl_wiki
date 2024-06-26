9b5fae7b6f8ac8620f78b4fc2277d009913afde7
miku-wwl <569144003@qq.com>
2024-06-26 22:34:33 +0800

    feat: 10-7 增加日志流水号，方便生产运维

--- src/main/java/com/weilai/wiki/job/DocJob.java
+++ src/main/java/com/weilai/wiki/job/DocJob.java
@@ -1,9 +1,11 @@
 package com.weilai.wiki.job;
 
 import com.weilai.wiki.service.DocService;
+import com.weilai.wiki.util.SnowFlake;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +16,16 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 每30秒更新电子书信息
      */
     @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         docService.updateEbookInfo();
     }
 
